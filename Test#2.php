3a06f01c7bfd06be08a5b44ab78863e78818d9e3
Brenthjerico <bjaybaldiviso@gmail.com>
2024-01-17 23:30:56 +0800

    Added Test#2.php

---
+++ Test#2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search</title>
+</head>
+<body>
+    <form action="search.php" method="GET">
+        <label for="search">Search:</label>
+        <input type="text" id="search" name="search">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
