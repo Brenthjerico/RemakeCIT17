6096c59047b34b114d58b91eda8b276e9dc6665d
Brenthjerico <bjaybaldiviso@gmail.com>
2024-01-19 00:41:05 +0800

    Added The Table_edit.php Final

--- Table_edit.php
+++ Table_edit.php
@@ -13,7 +13,7 @@
             $servername = "localhost";
             $username = "root";
             $password = "";
-            $dbname = "tableedit"
+            $dbname = "mosawan"
 
             $conn = new mysqli($servername, $username, $password, $dbname);
 
